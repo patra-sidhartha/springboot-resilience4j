409496f9c62c4b454389a86c1ff837e68b810d27
Sidhartha <sidharthapatra5@gmail.com>
2025-12-16 22:14:26 +0530

    circuit breaker example

---
+++ catalog-service/src/main/java/com/javatechie/os/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.st.os.repository;
+
+import com.st.os.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order,Integer> {
+
+    List<Order> findByCategory(String category);
+
+
+}
